0dc8b63e3bcc72019876f82a75feddea361ceb23
Brody Whalen <brody.whalen.1@gmail.com>
2025-04-06 04:10:40 -0500

    Initial Commit

---
+++ Stanford-Card-Game/Stanford_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_Card_GameApp.swift
+//  Stanford-Card-Game
+//
+//  Created by Brody Whalen on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
