0b7a7fba90a674dc8c8aa0eb6e7fb320116ff412
Brody Whalen <brody.whalen.1@gmail.com>
2025-04-07 23:21:04 -0500

    Add state changes

--- Stanford-Card-Game/ContentView.swift
+++ Stanford-Card-Game/ContentView.swift
@@ -23,30 +23,40 @@ struct ContentView: View {
 }
 
 struct CardView : View {
-    var isFaceUp: Bool = false
+    // cannot be let since the cardview will set.
+    @State var isFaceUp: Bool = false
     var body: some View {
-        if isFaceUp {
-            ZStack (content  : {
-                RoundedRectangle(cornerRadius: 12)
+        
+        //view modifier to make tap change
+        
+        ZStack () {
+            // let means the variable will never change, var will change
+            // type inference
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base
                     .foregroundStyle(.white)
-                RoundedRectangle(cornerRadius: 12)
+                base
                     .strokeBorder(lineWidth: 10)
                 Text("Hello, world!")
                     .font(.largeTitle)
                     .foregroundColor(.black)
-
-            })
-        } else {
-           RoundedRectangle(cornerRadius: 12)
-        }
-
+            } else {
+                base //.fill is standard
+            }
+         
+            
+        } // tap gesture logs taps when you click on the vstack
+        .onTapGesture(perform: {
+            // temporary state, for small animation things, not saving game state ect...
+            //            isFaceUp = !isFaceUp
+            // has toggle since it is a struct that has functions on it
+            //View are immutable! they cannot be changed
+            isFaceUp.toggle()
+        })
+                
     }
 }
-
-
-
-
-
 
 #Preview {
     ContentView()
